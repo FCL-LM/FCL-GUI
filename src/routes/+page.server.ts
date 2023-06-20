c7ebf4eda2a88015e72e05e5d21e65a7aaf99aa2
nebuchadneZZar01 <mykael98@live.it>
2023-06-20 16:07:56 +0200

    refactor: cleaned server code

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,15 +1,5 @@
 import { type Actions } from '@sveltejs/kit';
-import { config } from '$lib/config';
-import { getMessageStore } from '$lib/utils/getMessageStore';
-
-// Defined outside of the actions so it's not re-created on every request
-const messageStore = getMessageStore();
 
 export const actions: Actions = {
-  default: async ({ request }) => {
-    const formData = await request.formData();
-    const message = formData.get('message');
-
-    const prompt = `${config.promptPrefix}${message}`;
-  }
+  default: async () => {}
 };
